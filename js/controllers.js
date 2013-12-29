07817b06cc0672eeddb5c3904044f777fb97524f
Cosme <sharkoz@gmail.com>
2013-12-30 00:47:11 +0100

    Major overhaul

--- js/controllers.js
+++ js/controllers.js
@@ -2,12 +2,20 @@
 /* Controllers */
 
 
-function AppCtrl( $scope, $localStorage, $route, Geomath, Locate, InitDB){
+function AppCtrl( $scope, $localStorage, $route, Geomath, Locate){
 
+	// 1 - Get localstorage
     $scope.$storage = $localStorage;
-    console.log("Local Storage:")
-    console.log($scope.$storage.param)
+    //console.log("Local Storage:")
+    //console.log($scope.$storage.param)
 
+	if ($scope.$storage.max===undefined)
+	{
+		$scope.$storage.max = 5;
+	}
+	$scope.max = $scope.$storage.max;
+	
+	// 2 - Check if accessed via web or via PhoneGap app and change api URL accordingly
     var nativeapp = document.URL.indexOf( 'http://' ) === -1 && document.URL.indexOf( 'https://' ) === -1;
 	if ( nativeapp ) {
 	    // PhoneGap application
@@ -22,40 +30,35 @@ function AppCtrl( $scope, $localStorage, $route, Geomath, Locate, InitDB){
         $route.reload();
     };
 
-        $scope.setLoc = function(position) {
+    $scope.setLoc = function(position) {
       var coords = position.coords || position.coordinate || position;
       $scope.localise = true;
       $scope.pos= {'latitude' : coords.latitude, 'longitude' : coords.longitude};
       $scope.$storage.pos = $scope.pos;
         console.log($scope.pos);
         $scope.reloadRoute();
-        //console.log($scope.dataSet);
-
-        //for (var i=0;i<$scope.param.trajet.length;i++)
-        //{
-        //    console.log($scope.GareLoc[$scope.param.trajet[i].depart]);
-        //    $scope.param.trajet[i].dist = Geomath.calculateDistance($scope.GareLoc[$scope.param.trajet[i].depart],$scope.pos);
-        //    console.log($scope.param.trajet[i].dist);
-        //}
     }
 
-    $scope.localise = false;
-    $scope.pos = $scope.$storage.pos;
-        $scope.locate = Locate;
     $scope.calculateDistance = function(geo1, geo2){
         return Geomath.calculateDistance(geo1, geo2);
     }
+	
     $scope.local = function(){
         Locate.doGeolocation($scope.setLoc);
     }
+	
+	// At init, coordinates are not refreshed
+    $scope.localise = false;
+	if ($scope.$storage.pos===undefined)
+	{
+		$scope.$storage.pos = { "latitude" : 48.8753578, "longitude" : 2.3247332};
+	}
+    $scope.pos = $scope.$storage.pos;
+    //$scope.locate = Locate;
+	
+	// Refresh coordinates
     $scope.local();
-        $scope.max = $scope.$storage.max;
-
-		
-// NEW DB
-
-	$scope.db = InitDB;
-
+    
 }
 
 function HorairesCtrl( $scope, LibGare, Param){
@@ -65,8 +68,6 @@ function HorairesCtrl( $scope, LibGare, Param){
 if ($scope.$storage.param===undefined)
 {
     $scope.$storage.param = Param.values;
-  	//console.log("Initiate Local Storage:")
-    //console.log($scope.param) 
 }
 
     $scope.param = $scope.$storage.param;
@@ -82,37 +83,70 @@ if ($scope.$storage.param===undefined)
     //console.log($scope.ListeGares) 
 
     $scope.addTrajet = function(){
-        $scope.param.trajet.push({'depart' : '87382440' , 'arrivee' : '87384008', 'path' : 'previ'});
+        $scope.param.trajet.push({'depart' : 'BGV' , 'arrivee' : 'SNB', 'path' : 'mobil'});
         setTimeout(window.scrollTo(0,document.body.scrollHeight),500);
     }
 
 }
 
-function TrajetCtrl( $scope, DataSource ){
+function TrajetCtrl( $scope, DataSource, Getprevi ){
 
-    //This is the callback function
+    $scope.trajet.dist = $scope.calculateDistance($scope.GareLoc[$scope.trajet.depart],$scope.pos);
+
+	merge = function(live, previ){
+		var liv;
+		var pre;
+		for(liv=0; liv<live.length; ++liv){
+			for(pre=0; pre<previ.length; ++pre){
+				if(live[liv].num == previ[pre].num){
+					previ[pre].date.mode='R';
+					previ[pre].date.val=live[liv].date.val;
+					previ[pre].delta=(new Date('1970/01/01 '+previ[pre].date.val+':00')-new Date('1970/01/01 '+live[liv].date.val+':00'))/60;
+				}
+			}
+		}
+		return previ;
+	}
+	
+    //Sauvegarde pour toute la journée
+    $scope.saveData = function(data) {
+        $scope.trajet.save = data.passages;
+		$scope.trajet.previ = $scope.getprevi($scope.trajet.save, $scope.max);
+		if($scope.dataSet!==undefined){
+		$scope.trajet.previ = merge($scope.dataSet.train, $scope.previ);
+		}//console.log('Horaires prévisionnels chargés.'+$scope.trajet.previ);
+        //console.log("dataSet : scop "+$scope.$id+".");
+    }
+
+         //console.log($scope.apiUrl+$scope.trajet.path+"/"+$scope.trajet.depart);
+    $scope.jsonSave = function(){DataSource.get($scope.saveData, $scope.apiUrl+"gtfs/"+$scope.trajet.depart+"/"+$scope.trajet.arrivee);};
+
+    $scope.jsonSave();
+	$scope.getprevi = Getprevi.get;
+	if($scope.trajet.save===undefined)
+	{
+		console.log('Aucune donnée en mémoire.');
+	}
+	else
+	{
+		$scope.trajet.previ = Getprevi.get($scope.trajet.save, $scope.max);
+		//console.log('Données mémorisées chargées ');
+		console.log($scope.trajet.previ);
+	}
+	
+	    //This is the callback function
     $scope.setData = function(data) {
         $scope.dataSet = data.passages;
+		console.log($scope.dataSet.train);
+		console.log($scope.trajet.previ);
+		$scope.trajet.previ = merge($scope.dataSet.train, $scope.trajet.previ);
 		//console.log("dataSet : scop "+$scope.$id+".");
-		console.log($scope.dataSet);
+		//console.log($scope.dataSet);
     }
 
          //console.log($scope.apiUrl+$scope.trajet.path+"/"+$scope.trajet.depart);
     $scope.jsoncall = function(){DataSource.get($scope.setData, $scope.apiUrl+$scope.trajet.path+"/"+$scope.trajet.depart);};
 
     $scope.jsoncall();
-
-    $scope.trajet.dist = $scope.calculateDistance($scope.GareLoc[$scope.trajet.depart],$scope.pos);
-
-    //Sauvegarde pour toute la journÃ©e
-    $scope.saveData = function(data) {
-        $scope.trajet.save = data.passages;
-        //console.log("dataSet : scop "+$scope.$id+".");
-    }
-
-         //console.log($scope.apiUrl+$scope.trajet.path+"/"+$scope.trajet.depart);
-    $scope.jsonSave = function(){DataSource.get($scope.saveData, $scope.apiUrl+"gtfs/"+$scope.trajet.depart);};
-
-    $scope.jsonSave();
-
+	
 }
--- js/services.js
+++ js/services.js
@@ -16,11 +16,470 @@ app.factory('DataSource', ['$http',function($http){
 .service('LibGare', function(){
 return { values : [
 {"key" : "BGV", "value" : "BOUGIVAL", "latitude" : "48.854223502592255", "longitude" : "2.132240468348696"},
-{"key" : "PSL", "value" : "GARE ST LAZARE", "latitude" : "48.87535783596312", "longitude" : "2.3247332774874016"}
+{"key" : "PSL", "value" : "PARIS SAINT-LAZARE", "latitude" : "48.87535783596312", "longitude" : "2.3247332774874016"}
 ],
 func : {
-'BGV' : 'BOUGIVAL',
-'PSL' : 'PARIS SAINT-LAZARE'},
+'AB'  : 'Aulnay sous Bois',
+'ABL' : 'Ablon',
+'ACW' : 'Achères-Ville',
+'AEE' : 'Asnières sur Seine',
+'AEH' : 'Aubergenville Elisabethville',
+'AHM' : 'Avenue Henri Martin',
+'ALC' : 'Aubervilliers La Courneuve',
+'ANY' : 'Andrésy',
+'APK' : 'Avenue du Président Kennedy',
+'ARK' : 'Arcueil Cachan',
+'ARP' : 'Arpajon',
+'ARW' : 'Argenteuil',
+'ATH' : 'Athis Mons',
+'ATW' : 'Antony',
+'AUU' : 'Auber',
+'AUW' : 'Auvers-sur-Oise',
+'AVF' : 'Avenue Foch',
+'BAM' : 'Blanc Mesnil',
+'BBN' : 'La Borne Blanche',
+'BCO' : 'Bois Colombes',
+'BDE' : 'Sevran Beaudottes',
+'BDY' : 'Bondy' ,
+'BEC' : 'Bécon les Bruyères',
+'BFM' : 'Bibliothèque François Mitterrand',
+'BFX' : 'Le Bras de Fer',
+'BGK' : 'Bagneux',
+'BGV' : 'Bougival',
+'BIH' : 'Breuillet Bruyères le Chatel',
+'BIS' : 'Bièvres',
+'BJN' : 'Boutigny',
+'BJR' : 'Bois Le Roi',
+'BKR' : 'Boissise-le-Roi',
+'BLA' : "Boissy-l'Aillerie",
+'BLU' : 'Bellevue',
+'BNY' : 'Brunoy',
+'BOF' : 'Bouffemont Moisselles',
+'BOM' : 'Bourron Marlotte Grez',
+'BQA' : 'Bry sur Marne',
+'BQC' : 'Les Baconnets',
+'BQQ' : 'Bourg la Reine',
+'BQV' : 'Buno-Gironville',
+'BRK' : 'Bruyères-sur-Oise',
+'BRN' : 'Boran-sur-Oise',
+'BRW' : 'Breuillet Village',
+'BSO' : 'Bouray',
+'BSR' : 'Bessancourt',
+'BSW' : 'Belloy - Saint-Martin',
+'BUR' : 'Ballancourt',
+'BVI' : 'Boulevard Victor - Pont du Garigliano',
+'BVJ' : 'Bures sur Yvette',
+'BWI' : 'Boigneville',
+'BWR' : 'Boulainvilliers',
+'BXG' : 'Bussy Saint-Georges',
+'BXI' : 'Boussy Saint-Antoine',
+'BXN' : 'Bagneaux sur Loing',
+'BXP' : 'Les Boullereaux Champigny',
+'BXR' : 'Boissy Saint-Léger',
+'BY'  : 'Brétigny',
+'BYS' : 'Beynes',
+'CAZ' : 'Chilly Mazarin',
+'CBK' : 'Colombes',
+'CBV' : 'Combs la Ville Quincy',
+'CEG' : "Champ de Courses d'Enghien",
+'CEJ' : 'Cernay',
+'CES' : 'Cesson',
+'CEV' : 'Chaumont-en-Vexin',
+'CEX' : 'Le Coudray-Monceaux',
+'CFD' : "Conflans-Fin-d'Oise",
+'CGG' : 'Chenay Gagny' ,
+'CGJ' : 'Champigny',
+'CGP' : 'Charles de Gaulle-Étoile',
+'CGW' : 'Coignières',
+'CH'  : 'Chartres',
+'CHK' : 'Chamarande',
+'CHQ' : "Chemin d'Antony",
+'CHR' : 'Chars',
+'CHV' : 'Chaville Vélizy',
+'CJN' : 'Changis Saint-Jean',
+'CJR' : 'Chartrettes',
+'CJV' : 'Cergy le Haut',
+'CL'  : 'Creil',
+'CLC' : 'Crécy-la-Chapelle',
+'CLL' : 'Clichy Levallois',
+'CLR' : 'Choisy le Roi',
+'CLX' : 'Châtelet les Halles',
+'CLY' : 'Chantilly Gouvieux',
+'CMA' : 'Clamart',
+'CME' : 'Champagne sur Seine',
+'CO'  : 'Coulommiers',
+'COE' : 'Corbeil Essonnes',
+'COJ' : 'Compans',
+'CPA' : 'Cormeilles en Parisis',
+'CPK' : 'Champbenoist - Poigny',
+'CPM' : 'Champ de Mars Tour Eiffel',
+'CPO' : 'Champagne sur Oise',
+'CPW' : 'Chaponval',
+'CQQ' : 'Crouy-sur-Ourcq',
+'CSG' : 'Chelles Gournay',
+'CSH' : 'Conflans Sainte Honorine',
+'CTH' : 'Château Thierry',
+'CUF' : 'Cité Universitaire',
+'CVF' : 'Chanteloup les Vignes',
+'CVI' : 'Chaville Rive Gauche',
+'CVW' : 'Courcelles sur Yvette',
+'CWJ' : 'Chaville Rive Droite',
+'CXA' : 'Châtelet les Halles RER A',
+'CYC' : 'Cergy Saint-Christophe',
+'CYP' : 'Cergy Préfecture',
+'CYQ' : 'Couilly Saint-Germain Quincy',
+'CYV' : 'Crépy-en-Valois',
+'CYZ' : 'Chézy sur Marne',
+'D'   : 'Dourdan',
+'DA'  : 'Dourdan la Forêt',
+'DAM' : 'Dammartin Juilly-Saint-Mard',
+'DDI' : 'Dordives',
+'DEU' : 'Deuil Montmagny',
+'DFR' : 'Denfert Rochereau',
+'DMO' : 'Domont',
+'DRN' : 'Drancy',
+'DX'  : 'Dreux',
+'ECZ' : 'Écouen Ézanville',
+'ELW' : "L'Étang-la-Ville",
+'ELY' : 'Égly',
+'EM'  : 'Emerainville Pontault Combault',
+'EN'  : 'Enghien les Bains',
+'EPL' : 'Épluches',
+'EPN' : 'Épernon',
+'EPO' : 'Épone Mézières',
+'EPV' : 'Épinay Villetaneuse',
+'ERA' : 'Éragny Neuville',
+'ERM' : 'Ermont Halte',
+'ERT' : 'Ermont Eaubonne',
+'ESO' : 'Essonnes-Robinson',
+'ETP' : 'Étampes',
+'ETY' : 'Étrechy',
+'EVC' : 'Évry Courcouronnes',
+'EVR' : 'Évry',
+'EY'  : 'Esbly',
+'EYO' : 'Épinay sur Orge',
+'EYS' : 'Épinay sur Seine',
+'FAF' : 'Fontenay le Fleury',
+'FFY' : 'Ferrières Fontenay',
+'FMN' : 'Fontaine Michalon',
+'FMP' : 'Faremoutiers Pommeuse',
+'FMY' : 'La Frette Montigny',
+'FNR' : 'Fontenay aux Roses',
+'FON' : 'Fontainebleau Avon',
+'FPB' : 'Franconville Le Plessis Bouchard',
+'FPN' : 'Frépillon',
+'FPO' : 'Fontaine le Port',
+'FSB' : 'Fontenay-sous-Bois',
+'GAJ' : 'Garges Sarcelles',
+'GAQ' : 'Garancières la Queue',
+'GAW' : 'La Défense Grande Arche',
+'GBG' : 'Grand Bourg',
+'GBI' : 'Gravigny Balizy',
+'GCM' : 'Guérard la Celle sur Morin',
+'GCR' : 'Achères Grand Cormier',
+'GDS' : 'Paris Nord',
+'GEN' : 'Gennevilliers',
+'GGG' : 'Grigny Centre',
+'GGV' : 'Gargenville',
+'GIF' : 'Gif sur Yvette',
+'GIS' : 'Gisors',
+'GMC' : 'Garches Marnes-la-Coquette',
+'GN'  : 'Gagny' ,
+'GNX' : 'Gros Noyer Saint-Prix',
+'GOU' : 'Goussainville',
+'GPA' : 'La Grande Paroisse',
+'GRL' : 'Groslay',
+'GTL' : 'Gentilly',
+'GUW' : 'Le Guichet',
+'GYN' : 'Gare de Lyon RER A',
+'GZ'  : 'Gretz Armainvilliers' ,
+'GZA' : 'Gazeran',
+'HAQ' : 'La Hacquinière',
+'HAR' : 'Houilles Carrières sur Seine',
+'HER' : 'Héricy',
+'HOA' : 'Houdan',
+'HRY' : 'Herblay',
+'HSL' : 'Haussman Saint-Lazare' ,
+'IAC' : 'Isles-Armentières-Congis',
+'IAP' : "L'Isle Adam Parmain",
+'IBM' : 'Le Plessis-Chenet',
+'IGY' : 'Igny',
+'INV' : 'Invalides',
+'IPO' : 'Issou Porcheville',
+'ISP' : 'Issy Val de Seine',
+'ISY' : 'Issy',
+'IV'  : 'Ivry sur Seine',
+'JAS' : 'Jouy en Josas',
+'JOY' : 'Jouy',
+'JUZ' : 'Juziers',
+'JVL' : 'Javel',
+'JVR' : 'Joinville-le-Pont',
+'JY'  : 'Juvisy',
+'KOU' : 'Courbevoie',
+'KRW' : 'Montgeron Crosne',
+'KVE' : 'Les Clairières de Verneuil',
+'LAD' : 'Les Ardoines',
+'LBJ' : 'La Barre Ormesson',
+'LBT' : 'Le Bourget',
+'LCB' : 'La Celle-Saint-Cloud',
+'LDU' : 'La Défense',
+'LEG' : 'Les Grésillons',
+'LFA' : 'La Ferté Alais',
+'LFC' : 'Louveciennes',
+'LFJ' : 'La Ferté sous Jouarre',
+'LFM' : 'La Ferté Milon',
+'LGK' : 'La Garenne Colombes',
+'LGY' : 'Lagny Thorigny',
+'LIE' : "Saint-Ouen l'Aumône Liesse",
+'LIM' : 'Limay',
+'LIU' : 'Lieusaint Moissy',
+'LJA' : 'Laplace',
+'LJU' : 'Longjumeau',
+'LMU' : 'Les Mureaux',
+'LNX' : 'Les Noues',
+'LON' : 'Longueville', 
+'LOV' : 'Louvres',
+'LPE' : 'Le Perray',
+'LPN' : 'La Plaine Stade de France',
+'LQK' : 'Liancourt-Saint-Pierre',
+'LQN' : 'Lognes',
+'LSD' : 'Le Stade',
+'LSI' : 'Les Essarts le Roi',
+'LSW' : 'Les Saules',
+'LUZ' : 'Luzarches',
+'LVZ' : 'La Varenne Chennevières',
+'LWA' : 'Les Vallées',
+'LXJ' : 'Luxembourg',
+'LYO' : 'Lardy',
+'LYQ' : 'Livry sur Seine',
+'LYV' : 'Les Yvris Noisy Le Grand' ,
+'LZO' : 'Lizy-sur-Ourcq',
+'LZV' : 'Lozère',
+'MAE' : 'Maule',
+'MGT' : 'Magenta',
+'MAL' : 'Malesherbes',
+'MAQ' : 'Mareuil-sur-Ourcq',
+'MBP' : 'Montigny Beauchamp',
+'MBR' : 'Marchezais Broué',
+'MDN' : 'Meudon',
+'MDS' : "Musée d'Orsay",
+'MEA' : 'Meaux',
+'MEL' : 'Melun',
+'MFA' : 'Maisons Alfort Alfortville',
+'MFL' : 'Montreuil',
+'MFY' : 'Meudon Val Fleury',
+'MHD' : 'Meulan Hardricourt',
+'MJM' : 'Mareil sur Mauldre',
+'MJW' : 'Moulin-Galant',
+'MKN' : 'Montigny sur Loing',
+'MKU' : 'Montgeroult - Courcelles',
+'MLB' : 'Marles en Brie',
+'MLF' : 'Maisons Laffitte',
+'MLM' : "Montfort l'Amaury Méré",
+'MLR' : 'Marly-le-Roi',
+'MLV' : 'Meriel',
+'MNY' : 'Mennecy',
+'MOF' : 'Mortcerf',
+'MOR' : 'Morêt Veneux les Sablons',
+'MP'  : 'Massy Palaiseau RER B',
+'MPU' : 'Massy Palaiseau RER C',
+'MRK' : 'Maurecourt',
+'MRT' : 'Mormant',
+'MS'  : 'Montargis',
+'MSN' : 'Maisse',
+'MSO' : 'Montsoult Maffliers',
+'MSX' : 'Marolles en Hurepoix',
+'MTE' : 'Mantes la Jolie',
+'MTN' : 'Maintenon',
+'MTQ' : 'Mantes Station',
+'MTU' : 'Montereau',
+'MVC' : 'Marne la Vallée Chessy',
+'MVH' : 'Suresnes Mont Valérien',
+'MVP' : 'Massy Verrières RER B',
+'MVW' : 'Massy Verrières RER C',
+'MWO' : 'Méry sur Oise', 
+'MXK' : 'Mouroux',
+'MY'  : 'Mitry Claye',
+'MYD' : 'Montry Condé',
+'NAA' : "Nogent l'Artaud Charly",
+'NAF' : 'Nanterre Préfecture',
+'NAN' : 'Nangis',
+'NAU' : 'Nanteuil Saacy',
+'NC1' : 'Saint-Germain-en-Laye',
+'NC2' : 'Le Vésinet Le Pecq',
+'NC3' : 'Nanterre Ville',
+'NC4' : 'Le Vésinet-Centre',
+'NC5' : 'Chatou-Croissy',
+'NC6' : 'Rueil-Malmaison',
+'NG'  : 'La Norville Saint-Germain lès Arpajon',
+'NGM' : 'Nogent-sur-Marne',
+'NH'  : 'Nanteuil-le-Haudoin',
+'NLP' : 'Nogent Le Perreux' ,
+'NO'  : 'Nointel - Mours',
+'NPT' : 'Neuilly Porte Maillot',
+'NSL' : 'Noisiel',
+'NSP' : 'Nemours Saint-Pierre',
+'NSY' : 'Noisy Le Sec' ,
+'NTN' : 'Nation',
+'NUE' : 'Neuville Université',
+'NUN' : 'Nanterre Université',
+'NYC' : 'Noisy Champs',
+'NYG' : "Noisy le Grand Mont d'Est",
+'NYP' : 'Neuilly Plaisance',
+'NZL' : 'Nezel Aulnay',
+'OBP' : "Orangis Bois de l'Épine",
+'OGB' : 'Orgerus Behoust',
+'ORM' : 'Ormoy-Villers',
+'ORS' : 'Orsay Ville',
+'ORY' : 'Orry la Ville Coye',
+'OSN' : 'Osny',
+'OY'  : 'Orly Ville',
+'OZF' : 'Ozoir la Ferrière' ,
+'PLY' : 'Paris Gare de Lyon', 
+'PAA' : 'Paris Gare de Lyon',
+'PAN' : 'Pantin' ,
+'PAW' : 'Palaiseau Villebon',
+'PAX' : 'Palaiseau',
+'PCX' : 'Parc de Sceaux',
+'PDM' : "Pont de l'Alma",
+'PE'  : 'Paris Est',
+'PEB' : 'Persan Beaumont',
+'PEX' : 'Parc des Expositions',
+'PG'  : 'Plaisir Grignon',
+'PIE' : 'Plaisir les Clayes',
+'PJ'  : 'Petit Jouy les Loges',
+'PKY' : 'Porte de Clichy',
+'PLB' : 'Plessis-Belleville',
+'PMP' : 'Paris Montparnasse',
+'POA' : 'Porchefontaine',
+'POP' : 'Ponthierry-Pringy',
+'PPD' : 'Créteil Pompadour', 
+'PPT' : 'Pont-Petit',
+'PRF' : 'Pierrefitte Stains',
+'PRO' : 'Provins',
+'PRQ' : 'Presles - Courcelles',
+'PRR' : 'Pereire Levallois',
+'PRU' : "Pont de Rungis Aéroport d'Orly",
+'PRY' : 'Pierrelaye',
+'PSE' : 'Pontoise',
+'PSL' : 'Paris Saint-Lazare',
+'PSY' : 'Poissy',
+'PTC' : 'Pont Cardinet',
+'PTX' : 'Puteaux',
+'PV'  : 'Petit Vaux',
+'PWR' : 'Port Royal',
+'PXO' : 'Le Parc de Saint-Maur',
+'PYO' : 'Précy-sur-Oise',
+'PZB' : 'Paris Austerlitz',
+'RBI' : 'Rosny Bois Perrier' ,
+'RBT' : 'Rambouillet',
+'RF'  : 'Rungis la Fraternelle',
+'RIS' : 'Ris Orangis',
+'RNS' : 'Robinson',
+'ROB' : 'Roissy en Brie' ,
+'RSB' : 'Rosny sous Bois' ,
+'RSY' : 'Aéroport Charles de Gaulle 1',
+'RVM' : 'Le Raincy Villemomble' ,
+'RYR' : 'Aéroport Charles de Gaulle 2 TGV',
+'SAO' : 'Savigny sur Orge',
+'SCD' : 'Saint-Cloud',
+'SCR' : 'Saint-Cyr',
+'SCW' : 'Saint-Chéron',
+'SDE' : 'Saint-Denis',
+'SEV' : 'Sevran Livry',
+'SF'  : 'Saint-Mammes',
+'SFD' : 'Stade de France Saint-Denis',
+'SGT' : 'Saint-Gratien',
+'SHL' : 'Saint-Michel Notre Dame RER C', 
+'SHO' : 'Saint-Michel sur Orge',
+'SKX' : 'Sceaux',
+'SLF' : 'Saint-Leu la Forêt',
+'SLL' : 'Sarcelles Saint-Brice',
+'SLT' : "Saint-Leu d'Esserent",
+'SME' : "Saint-Martin d'Étampes",
+'SNB' : 'Saint-Nom-la-Bretèche - Forêt de Marly',
+'SNM' : 'Saint-Rémy lès Chevreuse',
+'SNN' : 'Sannois',
+'SOA' : "Saint-Ouen l'Aumône",
+'SOS' : 'Saint-Ouen',
+'SPI' : 'Saint-Piat',
+'SPP' : 'Souppes Château Landon',
+'SQY' : 'Saint-Quentin en Yvelines',
+'SUR' : 'Survilliers Fosses',
+'SVL' : 'Sartrouville',
+'SVR' : 'Sèvres Rive Gauche',
+'SWY' : 'Seugy',
+'SXE' : 'Sermaise',
+'SXG' : 'Sainte Geneviève des Bois',
+'TAE' : 'Tacoignières Richebourg',
+'TLP' : 'Trilport',
+'TMR' : 'Thomery',
+'TNT' : 'Thieux - Nantouillet',
+'TOC' : 'Torcy',
+'TOU' : 'Tournan',
+'TPA' : 'Thun le Paradis',
+'TRH' : 'Trie-Château',
+'TSS' : 'Triel sur Seine',
+'TVO' : 'Trappes',
+'TVY' : 'Taverny',
+'US'  : 'Us',
+'VAI' : 'Vaires Torcy',
+'VBB' : 'Villiers-Montbarbin',
+'VBO' : 'Vauboyen',
+'VBV' : 'Villabé',
+'VC'  : 'Versailles Chantiers',
+'VCN' : 'Vaucresson',
+'VCX' : 'Vaucelles',
+'VD'  : 'Le Vert de Maisons',
+'VDA' : "Val d'Argenteuil",
+'VDE' : "Val d'Europe",
+'VDF' : "Val de Fontenay RER E",
+'VDO' : "Le Val d'Or",
+'VDV' : "Sèvres Ville d'Avray",
+'VEH' : 'Villiers Neauphle Pontchartrain',
+'VEP' : 'Villepreux les Clayes',
+'VET' : 'Vernouillet Verneuil',
+'VFD' : 'Viroflay Rive Droite',
+'VFG' : 'Viroflay Rive Gauche',
+'VFR' : 'Val de Fontenay RER A',
+'VGL' : 'Vert Galant',
+'VGS' : 'Vigneux sur Seine',
+'VIB' : 'Villiers le Bel Gonesse Arnouville',
+'VII' : 'Villeparisis Mitry Le Neuf',
+'VMD' : 'Valmondois',
+'VMK' : 'Vanves Malakoff',
+'VMS' : 'Viarmes',
+'VNC' : 'Vincennes',
+'VNL' : "Verneuil l'Étang",
+'VOM' : 'Vosves',
+'VP'  : 'Villeneuve Prairie',
+'VPN' : 'Villepinte',
+'VRD' : 'Versailles Rive Droite',
+'VRG' : 'Versailles Rive Gauche Château',
+'VRI' : 'Villeneuve le Roi',
+'VSG' : 'Villeneuve Saint-Georges',
+'VSM' : 'Villiers sur Marne Plessis Trévise',
+'VSS' : 'Vernou sur Seine',
+'VSW' : 'Villennes sur Seine',
+'VTV' : 'Villeneuve Triage',
+'VUN' : 'Vulaines sur Seine Samoreau',
+'VW'  : 'Villaines',
+'VWC' : 'Viry Châtillon',
+'VWT' : 'La Villetertre',
+'VXS' : 'Vaux sur Seine',
+'VY'  : 'Vitry sur Seine',
+'VYL' : 'La Verrière',
+'WEE' : 'Le Mée',
+'XBY' : 'La Croix de Berny',
+'XCS' : 'Saint-Colombe - Septveilles',
+'XFA' : 'Saint-Fargeau',
+'XMC' : 'Saint-Maur-Créteil',
+'XND' : 'Saint-Michel Notre Dame RER B',
+'XOA' : "Saint-Ouen l'Aumône Quartier de l'Église",
+'XPY' : 'Santeuil - Le Perchay',
+'YES' : 'Yerres',
+'ZTN' : 'Savigny le Temple Nandy',
+'ZUB' : 'Sucy Bonneuil'},
 gareloc : {
 "BGV" : { "latitude" : 48.8542235, "longitude" : 2.1322404},
 "PSL" : { "latitude" : 48.8753578, "longitude" : 2.3247332}}
@@ -30,38 +489,71 @@ gareloc : {
 .service('Param', function(){
   return { 
     values : {trajet : [
-	{'depart' : 'BGV' , 'arrivee' : 'PSL', 'path' : 'previ'},
-    {'depart' : 'BGV' , 'arrivee' : 'PSL', 'path' : 'live'}
+	{'depart' : 'PSL' , 'arrivee' : 'SNB', 'path' : 'mobil'},
+    {'depart' : 'BGV' , 'arrivee' : 'PSL', 'path' : 'mobil'}
     ]}
   } 
 })
 
-.service('InitDB', function(){
- // Populate the database 
-    //
-    function populateDB(tx) {
-         tx.executeSql('DROP TABLE IF EXISTS DEMO');
-         tx.executeSql('CREATE TABLE IF NOT EXISTS DEMO (id unique, data)');
-         tx.executeSql('INSERT INTO DEMO (id, data) VALUES (1, "First row")');
-         tx.executeSql('INSERT INTO DEMO (id, data) VALUES (2, "Second row")');
-    }
-
-    // Transaction error callback
-    //
-    function errorCB(err) {
-        alert("Error processing SQL: "+err);
-    }
-
-    // Transaction success callback
-    //
-    function successCB() {
-        alert("success!");
-    }
-
-	var db = window.openDatabase("Database", "1.0", "Cordova Demo", 200000);
-        db.transaction(populateDB, errorCB, successCB);
+.service('Getprevi', function(){
+return {get : function(save, max){
+		var i=0;
+		var n=0;
+		var take=0;
+		var now = new Date(Date.now());
+		var time=('0'+now.getHours()).substr(-2)+''+('0'+now.getMinutes()).substr(-2);
+		var day = now.getFullYear()+'-'+('0'+(now.getMonth()+1)).substr(-2)+'-'+('0'+now.getDate()).substr(-2);
+		var dow = now.getDay();
+		var j;
+		var res = new Array;
+		var s=save.train.length;
+		//console.log('Init : long: '+s+' Time : '+time+' // day : '+day);
 		
-	return db;
+		while(n<max && take<3){
+		//while(i<s-3){
+			//console.log('reboot : i='+i+' , n='+n+' ,take='+take);
+			if(i>s-1){
+				i=0;
+				now = new Date(now.getFullYear(), now.getMonth(), now.getDate()+1);
+				day = now.getFullYear()+'-'+('0'+(now.getMonth()+1)).substr(-2)+'-'+('0'+now.getDate()).substr(-2);
+				dow = now.getDay();
+				//console.log('long: '+s+' Time : '+time+' // day : '+day);
+				take=take+1
+			}
+			j = save.train[i];
+			//console.log(j);
+			if(j.date.val.replace(":","") < time && take==0){
+				i=i+1;
+				//console.log('no / '+j.date.val.replace(":","")+' > '+time);
+			}
+			else {
+				//console.log(j.date.val.replace(":","")+' > '+time);
+				if(!!j.valid.moins && j.valid.moins.indexOf(day)>-1){
+					i=i+1;
+					//console.log('moins');
+				}
+				else {
+					if(((j.valid.deb.replace(/-/g,"") <= day.replace(/-/g,"")) && (j.valid.fin.replace(/-/g,"") >= day.replace(/-/g,"")) && (j.valid.mask.indexOf(dow) > -1)) || (!!j.valid.plus && j.valid.plus.indexOf(day) > -1)){
+						take = 1;
+						//console.log('take');
+						if(j.date.val>'23:59'){
+							j.date.val = j.date.val.substr(0,2)-24 + j.date.val.substr(2,3);
+						}
+						res.push(j);
+						n=n+1;
+						i=i+1;
+					}
+					else{
+						i=i+1;
+						//console.log(j.valid.deb.replace(/-/g,"")+' <= '+day.replace(/-/g,"")+' :'+(j.valid.deb.replace(/-/g,"") <= day.replace(/-/g,""))+' && '+j.valid.fin.replace(/-/g,"")+' >= '+day.replace(/-/g,"")+' && '+j.valid.mask.indexOf(dow));
+					}
+				}
+			}
+		}
+		//console.log(res);
+		return res;
+	}
+	}
 })
 
 .service('Geomath', function() {
@@ -159,7 +651,8 @@ gareloc : {
 })
 
 .service('Locate', function() {
-    var self = this;
+    
+	var self = this;
 
     this.doGeolocation = function(callback) {
       if (navigator.geolocation) {
@@ -173,19 +666,19 @@ gareloc : {
       var msg;
       switch(err.code) {
         case err.UNKNOWN_ERROR:
-          msg = "Impossible de vous gÃ©olocaliser";
+          msg = "Impossible de vous géolocaliser";
           break;
         case err.PERMISSION_DENINED:
-          msg = "Permission refusÃ©e pour la gÃ©olocalisation";
+          msg = "Permission refusée pour la géolocalisation";
           break;
         case err.POSITION_UNAVAILABLE:
           msg = "Votre position n'est pas disponible";
           break;
         case err.BREAK:
-          msg = "La gÃ©olocalisation a mis trop de temps";
+          msg = "La géolocalisation a mis trop de temps";
           break;
         default:
-          msg = "Votre appareil ne supporte pas la gÃ©olocalisation";
+          msg = "Votre appareil ne supporte pas la géolocalisation";
       }
     console.log(msg);
     }
