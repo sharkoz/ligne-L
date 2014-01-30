c376c82ec82b29bf394537908b563c2a1ddf7448
Cosme <sharkoz@gmail.com>
2014-01-30 14:21:41 +0100

    minor_fix

--- js/app.js
+++ js/app.js
@@ -9,4 +9,4 @@ app.config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
-
+*/
