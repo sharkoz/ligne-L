67b19a0762716e6277fc4c6b22c19021b5b85c8b
Cosme <sharkoz@gmail.com>
2014-02-10 23:18:29 +0100

    fixed_issue_#3

--- js/app.js
+++ js/app.js
@@ -10,3 +10,26 @@ app.config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
 */
+
+app.directive('clock', function($timeout, dateFilter){
+    return function(scope, element, attrs){
+       var timeoutId; // timeoutId, so that we can cancel the time updates
+ 
+      // schedule update in one second
+      function updateLater() {
+        // save the timeoutId for canceling
+        timeoutId = $timeout(function() {
+          element.text(dateFilter(new Date(), 'HH:mm:ss '));
+          updateLater(); // schedule another update
+        }, 1000);
+      }
+        
+      // listen on DOM destroy (removal) event, and cancel the next UI update
+      // to prevent updating time ofter the DOM element was removed.
+      element.bind('$destroy', function() {
+        $timeout.cancel(timeoutId);
+      });
+ 
+      updateLater(); // kick off the UI update process.
+    }
+});
